58422698d04d063144fb9bd13e3a7c4e9bcdbb77
bekirasimcelik <bekirasimcelik@hotmail.com>
2024-07-26 00:04:06 +0300

    shopping cart updated

--- script.js
+++ script.js
@@ -67,9 +67,23 @@ class UI {
           cart = [...cart, cartItem];
           // save cart in local storage
           Storage.saveCart(cart);
+          // save cart values
+          this.saveCartValues(cart);
         });
       }
     });
+  }
+
+  saveCartValues(cart) {
+    let tempTotal = 0;
+    let itemsTotal = 0;
+    cart.map((item) => {
+      tempTotal += item.price * item.amount;
+      itemsTotal += item.amount;
+    });
+
+    cartTotal.innerText = parseFloat(tempTotal.toFixed(2));
+    cartItems.innerText = itemsTotal;
   }
 }
 
